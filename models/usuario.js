ac6efb5b4b4e266d21c84cd809a84ae7d85ca327
T4RUM <wesley-murat@hotmail.com>
2024-07-31 17:41:15 -0300

    Adicionando Models

---
+++ models/usuario.js
@@ -0,0 +1,16 @@
+import { connection } from "../config/database.js";
+import { DataTypes } from "sequelize"; 
+
+
+export const Usuario = connection.define("usuario", {
+    
+    nome: {
+        type: DataTypes.STRING(130), 
+        allowNull: false 
+    },
+    email: { 
+        type: DataTypes.STRING, 
+        allowNull: false,
+        unique: true
+    }
+});
