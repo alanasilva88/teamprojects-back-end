93f5ed90b151737bce7c140f435ff06968f48359
T4RUM <wesley-murat@hotmail.com>
2024-08-01 15:17:57 -0300

    Alreação na model usuario

--- index.js
+++ index.js
@@ -6,28 +6,18 @@ import { Tarefa } from "./models/tarefa.js";
 import { Usuario } from "./models/usuario.js";
 import express from "express";
 
-
 authenticate(connection).then(() => {
-    connection.sync(); 
-  
+  connection.sync();
 });
-
 
 const app = express();
 
+app.use(express.json());
 
-app.use(express.json()); 
-
-app.get("/hello", (requisicao, resposta) => { 
-    resposta.send("Hello World!"); 
+app.get("/hello", (requisicao, resposta) => {
+  resposta.send("Hello World!");
 });
 
-
-
-
-
-
-
 app.listen(4000, () => {
-    console.log("Servidor rodando em http://localhost:4000/");
-  });
+  console.log("Servidor rodando em http://localhost:4000/");
+});
