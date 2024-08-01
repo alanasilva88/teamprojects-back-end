e17eaa78736cb56c3bd080f2e8564b2e6db07294
alanasilva88 <alanasilva.as@gmail.com>
2024-08-01 08:58:44 -0300

    criação da rota e express

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ authenticate(connection).then(() => {
     connection.sync();
 
 });
+
+const app = express();
+
+
+app.use(express.json()); 
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("Servidor rodando em http://localhost:3000/");
+  });
